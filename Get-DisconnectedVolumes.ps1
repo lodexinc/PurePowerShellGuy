29a1bc6c3893abd427e92fb5ab015ae502797b2c
barkz <barkz@purestorage.com>
2016-07-25 18:37:50 -0700

    Get-DisconnectedVolumes Script

---
+++ Get-DisconnectedVolumes.ps1
@@ -0,0 +1,34 @@
+﻿$FlashArrayID = Read-Host -Prompt "Enter the Pure Storage FlashArray (IP/FQDN)" 
+$FlashArray = New-PfaArray -EndPoint 10.21.10.17 -Credentials (Get-Credential) -IgnoreCertificateError
+
+$ConnectedVolumes = @($null)
+$AllVolumes = @($null)
+$DisconnectedVolumes = @($null)
+$z=0
+
+$Hosts = Get-PfaHosts -Array $FlashArray
+ForEach ($HostVol in $Hosts) {
+    $ConnectedVolumes += @(Get-PfaHostVolumeConnections -Array $FlashArray -Name $HostVol.name | select vol)
+}
+
+$AllVolumes = @(Get-PfaVolumes -Array $FlashArray | select name)
+$hash= @{}
+
+foreach ($i in $ConnectedVolumes) {
+    $Vol = $i.vol
+    $hash.Add($z, $Vol)
+    $z++
+}
+
+foreach($j in $AllVolumes) {
+   if(!$hash.ContainsValue($j.name)){
+        $DisconnectedVolumes += $j.name
+    }
+    else {
+        $hash.Remove($j.name)
+    }
+}
+
+$DisconnectedVolumes
+
+
